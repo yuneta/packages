0a538ffd1cc30984df7c63ac557e42344a0d4ed0
Niyamaka <niyamaka@yuneta.io>
2020-06-09 09:09:48 +0200

    set in js and package scripts the version 4.1.3

--- build-yuneta-agent-debian-ARM32.sh
+++ build-yuneta-agent-debian-ARM32.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PROYECT="yuneta-agent"
-VERSION="3.9.0"
+VERSION="4.1.3"
 RELEASE="1"
 ARCHITECTURE="arm32"
 
--- build-yuneta-agent-rpm.sh
+++ build-yuneta-agent-rpm.sh
@@ -19,7 +19,7 @@
 #
 
 
-VERSION="3.9.0"
+VERSION="4.1.3"
 RELEASE="1"
 DATE="Fri Jan 27 2017" TODO
 
