93789df7cd85a670c94124d7b32172e1d6050eab
Niyamaka <niyamaka@yuneta.io>
2020-06-17 00:52:07 +0200

    v4.1.4

--- build-yuneta-agent-debian-AMD64.sh
+++ build-yuneta-agent-debian-AMD64.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PROYECT="yuneta-agent"
-VERSION="4.1.3"
+VERSION="4.1.4"
 RELEASE="1"
 ARCHITECTURE="amd64"
 
--- build-yuneta-agent-rpm.sh
+++ build-yuneta-agent-rpm.sh
@@ -19,7 +19,7 @@
 #
 
 
-VERSION="4.1.3"
+VERSION="4.1.4"
 RELEASE="1"
 DATE="Fri Jan 27 2017" TODO
 
