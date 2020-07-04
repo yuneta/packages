ec25b037ed0b439fa5d104fe9fb6e3ad109a2f05
Niyamaka <niyamaka@yuneta.io>
2020-07-04 12:45:36 +0200

    v4.2.1 fix websocket and httparser

--- build-yuneta-agent-raspberrypi-ARMhf.sh
+++ build-yuneta-agent-raspberrypi-ARMhf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PROYECT="yuneta-agent"
-VERSION="4.2.0"
+VERSION="4.2.1"
 RELEASE="1"
 ARCHITECTURE="armhf"
 
--- build-yuneta-agent-rpm.sh
+++ build-yuneta-agent-rpm.sh
@@ -19,7 +19,7 @@
 #
 
 
-VERSION="4.2.0"
+VERSION="4.2.1"
 RELEASE="1"
 DATE="Fri Jan 27 2017" TODO
 
