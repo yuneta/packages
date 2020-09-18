a9979afd03b0e21dd1ae365ec1e284c95844888d
Niyamaka <niyamaka@yuneta.io>
2020-09-18 12:27:39 +0200

    v4.2.14

--- build-yuneta-agent-debian-ARM32.sh
+++ build-yuneta-agent-debian-ARM32.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PROYECT="yuneta-agent"
-VERSION="4.2.13"
+VERSION="4.2.14"
 RELEASE="1"
 ARCHITECTURE="arm32"
 
--- build-yuneta-agent-rpm.sh
+++ build-yuneta-agent-rpm.sh
@@ -19,7 +19,7 @@
 #
 
 
-VERSION="4.2.13"
+VERSION="4.2.14"
 RELEASE="1"
 DATE="Fri Jan 27 2017" TODO
 
